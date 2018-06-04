dafd552ffba913cb0caf4252ead05824221f645a
Konrad Ryczek <konradryczek92@gmail.com>
2018-06-04 14:26:13 +0200

    Add applyDiscount prototype in script.js

--- js/script.js
+++ js/script.js
@@ -8,10 +8,18 @@ Phone.prototype.printInfo = function() {
 	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ', and the price is ' + this.price + '.');
 };
 
-var SamsungGalaxyS6 = new Phone('Samsung', 2000, 'black'),
+var samsungGalaxyS6 = new Phone('Samsung', 2000, 'black'),
 	iPhone6S = new Phone('Apple', 2400, 'white'),
-	OnePlusOne = new Phone('OnePlus', 1900, 'green');
+	onePlusOne = new Phone('OnePlus', 1900, 'green');
 
-SamsungGalaxyS6.printInfo();
+samsungGalaxyS6.printInfo();
 iPhone6S.printInfo();
-OnePlusOne.printInfo();
+onePlusOne.printInfo();
+
+Phone.prototype.applyDiscount = function(discount) {
+	this.price = this.price - discount;
+};
+
+iPhone6S.applyDiscount(200);
+
+iPhone6S.printInfo();
